0b9d76aab671a1f1f0fcb6ea76e89b35989ea9e5
Aleksej Taratuta <116726052+Liolikmolo@users.noreply.github.com>
2026-03-20 11:09:45 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,12 +38,18 @@ export default function RootLayout({
         <html lang = "ru" suppressHydrationWarning>
             <head>
                 {/* <!-- Google Tag Manager --> */}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N3X82MV5');</script>
-{/* <!-- End Google Tag Manager --> */}
+                    <script>
+                        (function(w,d,s,l,i)
+                        { w[l]=w[l]||[]
+                        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                        const f= d.getElementsByTagName(s)[0], j= d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                        j.async=true;
+                        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                        f.parentNode.insertBefore(j,f);
+                        })
+                        (window,document,'script','dataLayer','GTM-N3X82MV5');
+                    </script>
+                {/* <!-- End Google Tag Manager --> */}
                 <meta name="google-site-verification" content="-gHHno-uqJ7dEmK2J_iREdewXLFVEiMDNrQSZJWr920" />
             </head>
         <body className = {manrope.className}>
